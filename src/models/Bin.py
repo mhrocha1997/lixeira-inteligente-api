1758248af3a02025bd0373722b356d02a442c58c
Marcelo <marcelo.h.rocha1997@gmail.com>
2021-04-10 12:52:11 -0300

    [FIX] Bin imports

--- src/models/Bin.py
+++ src/models/Bin.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, Text, DateTime
 from sqlalchemy.orm import relationship
 
 from database import Base
 
 class Bin(Base):
+    __tablename__ = "bins"
 
-    id_bin = Column(INTEGER(unsigned=True), primary_key=True)
+    id_bin = Column(Integer, primary_key=True)
     address = Column(String(70), nullable=False)
-    capacity = Column(INTEGER(unsigned=True), nullable=False)
-    status = Column(INTEGER(unsigned=True), nullable=False)
+    capacity = Column(Integer, nullable=False)
+    status = Column(Integer, nullable=False)
     last_updated = Column(DateTime, nullable=False)
     hub_id = Column(Integer, ForeignKey("hubs.id"))
 
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .User import User
 from .Product import Product
 from .Hub import Hub
+from .Bin import Bin
